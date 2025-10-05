b28650600a69121b1534a3f85fc74354d858d5e1
Joseph Salas <josephgsalas@gmail.com>
2025-10-05 14:05:48 -0600

    cleanup ks and cleanup

--- packer/scripts/cleanup.sh
+++ packer/scripts/cleanup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
-rm -rf /tmp/* /var/cache/{yum,dnf}/* || true
+rm -rf /tmp/* /var/cache/{dnf,yum}/* || true
 rm -f /etc/ssh/ssh_host_* || true
 dd if=/dev/zero of=/EMPTY bs=1M || true
 rm -f /EMPTY
